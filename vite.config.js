c1aef0bed7fd10f4b51f97650e11cf8272ecbfc6
Jimmy <johnny@kludgebot.com>
2025-11-09 02:09:09 +0000

    Configure Vite base path for dev proxy access

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/api/proxy/tonescope/dev/',
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     host: true
